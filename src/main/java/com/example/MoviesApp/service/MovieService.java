650582adbd6833bfb33f7e4cf6ec8d970605bf49
Mohammed Arshath <sm.arshath2003@gmail.com>
2025-01-31 12:42:02 +0530

    new features added

--- src/main/java/com/example/MoviesApp/service/MovieService.java
+++ src/main/java/com/example/MoviesApp/service/MovieService.java
@@ -193,7 +193,7 @@ public class MovieService {
         return movies;
     }
 
-    @Scheduled(cron = "0 40 12 * * ?")
+    @Scheduled(cron = "0 0 13 * * ?")
     public List<Movie> fetchAndStoreTodayReleases() {
         logger.info("Fetching today's new releases...");
         String today = LocalDate.now().toString();
