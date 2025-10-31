695fb4f1367e7f77ef4ce9faef9d99b98ebc97b4
mohammedarshath2705 <sm.arshath2003@gmail.com>
2025-10-31 21:10:05 +0530

    changed App properties

--- src/main/java/com/example/MoviesApp/configuration/RedisConfig.java
+++ src/main/java/com/example/MoviesApp/configuration/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.example.MoviesApp.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -18,17 +19,21 @@ import java.time.Duration;
 @EnableCaching
 public class RedisConfig {
 
+    @Value("${spring.redis.host:localhost}")
+    private String redisHost;
+
+    @Value("${spring.redis.port:6379}")
+    private int redisPort;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        // You can configure SSL, connection pooling, etc. here
-        return new LettuceConnectionFactory("red-cudm0olumphs73cpn9lg", 6379);
+        return new LettuceConnectionFactory(redisHost, redisPort);
     }
 
     @Bean
     public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
         RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofMinutes(10))
-                // .entryTtl(Duration.ofHours(24))
                 .disableCachingNullValues();
 
         return RedisCacheManager.builder(redisConnectionFactory)
@@ -41,7 +46,7 @@ public class RedisConfig {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericToStringSerializer<Object>(Object.class));
+        template.setValueSerializer(new GenericToStringSerializer<>(Object.class));
         return template;
     }
 }
